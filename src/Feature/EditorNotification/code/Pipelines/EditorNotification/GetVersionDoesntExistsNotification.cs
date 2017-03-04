b223f7f3848dc00d6a928328e4b67222f44222ae
Chaturanga Ranatunga <chaturangar@gmail.com>
2017-03-04 15:20:04 +0530

    after setting up feature projects

---
+++ src/Feature/EditorNotification/code/Pipelines/EditorNotification/GetVersionDoesntExistsNotification.cs
@@ -0,0 +1,16 @@
+﻿using Sitecore.Pipelines.GetPageEditorNotifications;
+
+namespace Sitecore.Feature.EditorNotification.Pipelines.EditorNotification
+{
+    public class GetVersionDoesntExistsNotification : GetPageEditorNotificationsProcessor
+    {
+        public override void Process(GetPageEditorNotificationsArgs arguments)
+        {
+            var description = "If you publish now, the current item may not render properly because the following items have the same name. Click the links below to review these items:";
+
+            var editorNotification = new PageEditorNotification(description, PageEditorNotificationType.Warning);
+
+            arguments.Notifications.Add(editorNotification);
+        }
+    }
+}
