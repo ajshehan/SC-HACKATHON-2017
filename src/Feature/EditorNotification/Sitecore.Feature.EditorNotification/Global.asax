59ee0561b80dfe6a1c0fb6a5b86ed26f2cbf3a35
ajshehan <sas@codehousegroup.com>
2017-03-04 12:59:45 +0530

    initial creation of the feature projects

---
+++ src/Feature/EditorNotification/Sitecore.Feature.EditorNotification/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Sitecore.Feature.EditorNotification.MvcApplication" Language="C#" %>
---
+++ src/Feature/EditorNotification/Sitecore.Feature.EditorNotification/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Sitecore.Feature.EditorNotification.Startup))]
+namespace Sitecore.Feature.EditorNotification
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
